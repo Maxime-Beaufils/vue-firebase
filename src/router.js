3f73d2036cea4db803725e00bd6d737d32f52fe8
Maxime-Beaufils <thelimier@gmail.com>
2019-09-01 17:21:11 +0200

    add route

--- src/router.js
+++ src/router.js
@@ -1,25 +1,40 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import firebase from 'firebase';
+
+import Login from '@/components/Login.vue';
+import Dashboard from '@/components/Dashboard.vue';
+import Settings from '@/components/Settings.vue';
 
 Vue.use(Router);
 
 export default new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home,
+      path: '*',
+      redirect: '/dashboard',
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/login',
+      name: 'Login',
+      component: Login,
+    },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: Dashboard,
+      meta: {
+        requiresAuth: true,
+      },
+    },
+    {
+      path: '/settings',
+      name: 'Settings',
+      component: Settings,
+      meta: {
+        requiresAuth: true,
+      },
     },
   ],
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <p>yolo</p>
   </div>
 </template>
 
